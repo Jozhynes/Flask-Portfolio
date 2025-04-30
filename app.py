c7b815bb6c5ebe8a98d36f620b9d109cbda14ef0
Jozhynes <jozhynespatrick@gmail.com>
2025-04-30 19:13:48 +0300

    App

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask,url_for,render_template
+from flask import Flask,url_for,render_template,request
 
 app=Flask(__name__)
+Mylist=[]
 
 @app.route('/')
-def home():
+def Home():
     
-    return render_template('index.html')
+    return render_template('index.html',products=Mylist)
+
+
+@app.route('/contacts')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/Products', methods=["POST", "GET"])
+def product():
+    if request.method == "POST":
+        pname = request.form['pname']
+        quantity = request.form['Quantity']
+        price = request.form['Price']
+        
+        # Append dictionary to list
+        Mylist.append({
+            'Product_name': pname,
+            'Quantity': quantity,
+            'Price': price
+        })
+    print(Mylist)
+    
+        
+
+
+
 
 
 if __name__=="__main__":
